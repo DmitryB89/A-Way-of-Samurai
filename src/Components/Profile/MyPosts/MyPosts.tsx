e41cca6f148ecfddc4acf5a1b133387f417398f0
Dmitry <d.baydarov@yandex.ru>
2022-10-30 00:39:40 +0300

    first props

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
+
+
+
 export const MyPosts = () => {
     return (
         <div>
@@ -11,7 +14,8 @@ export const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-               <Post/>
+               <Post message={'How are you?'}/>
+               <Post message={'It is my first post'}/>
             </div>
         </div>
     )
--- src/Components/Profile/MyPosts/Post/Post.tsx
+++ src/Components/Profile/MyPosts/Post/Post.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import s from './Post.module.css'
 
-export const Post = () => {
+type PostPropsType = {
+    message:string
+}
+
+export const Post:React.FC<PostPropsType> = (props) => {
     return (
         <div className={s.item}>
             <img src="https://krasivosti.pro/uploads/posts/2021-04/1617927302_46-p-kotik-za-kompyuterom-54.jpg"
                  alt="ava"/>
-            post 1
+            {props.message}
             <div>
                 <span>like</span>
             </div>
