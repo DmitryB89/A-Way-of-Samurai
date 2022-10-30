9788cce8c0b86480f7a33da8bd751b6b9c3f8ba4
Dmitry <d.baydarov@yandex.ru>
2022-10-30 16:34:24 +0300

    Dialogs page, Navlinks

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,14 +1,39 @@
 import React from "react";
 import s from './Dialogs.module.css'
-type DialogsPropsType = {
+import {NavLink} from "react-router-dom";
 
-}
+type DialogsPropsType = {}
 
 
-export const Dialogs:React.FC<DialogsPropsType> = (props) => {
+export const Dialogs: React.FC<DialogsPropsType> = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to={'/dialogs/1'}>Dimych</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/2'}>Alena</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/3'}>Andrey</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/4'}>Egor</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/5'}>Valera</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to={'/dialogs/6'}>Ksenia</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your progress?</div>
+                <div className={s.message}>Yo</div>
+            </div>
+
         </div>
     )
 }
