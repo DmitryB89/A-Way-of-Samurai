682e5748f3b42b18a4016cdc40f48aa834e79ac2
Dmitry <d.baydarov@yandex.ru>
2023-01-11 01:44:26 +0300

    auth-reducer

---
+++ src/Components/Header/HeaderContainer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
+
+
+
+
+export const Header = () => {
+    return (
+        <header className={s.header}>
+            <img
+                src="https://thumbnailer.mixcloud.com/unsafe/900x900/extaudio/e/2/1/5/6e6e-3195-4357-a11c-ae11da864c3a"
+                alt="logo"/>
+            <div className={s.loginBlock}>
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>
+        </header>
+    )
+}
+
